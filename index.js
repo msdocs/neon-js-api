58c8ea44462f9ee7507551f5b23ae2a227b92854
msdocs <msdocs@me.com>
2018-03-11 18:16:08 -0400

    Added getStorage

--- index.js
+++ index.js
@@ -10,14 +10,24 @@ const app = express()
 app.get('/', (req, res) => res.send('API Alpha 1'));
 
 app.get('/invokeContract/:task/:input', function(request, result) {
-    	    input = request.params.input.split(","); 
-	    	node.invokeContract(request.params.task,input, account, (res) => {
-	        if (res.result === true) {
-	            result.send('Transaction processed!');
-	        } else {
-	            result.send('Transaction Failed!');
-	        }
-	    })
+    input = request.params.input.split(",");
+    node.invokeContract(request.params.task, input, account, (res) => {
+        if (res.result === true) {
+            result.send('Transaction processed!');
+        } else {
+            result.send('Transaction Failed!');
+        }
+    })
 });
-app.listen(3000, () => console.log('API Server on Port 3000'));
+app.get('/getStorage/:key', function(request, result) {
+    key = request.params.key;
+    node.getStorage(key).then((res) => {
+        if (typeof res === "string") {
+           result.send(res);
+        } else{
+        	result.send("Error",res.result);
+        }
+    })
+});
 
+app.listen(3000, () => console.log('API Server on Port 3000'));
